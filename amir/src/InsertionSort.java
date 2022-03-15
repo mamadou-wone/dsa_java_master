534a9b90a64b144b33785a23ffc7a1213fa8d52d
Mamadou WONE <mamadu.wone@gmail.com>
2022-03-15 17:12:44 +0000

    for fun

---
+++ amir/src/InsertionSort.java
@@ -0,0 +1,14 @@
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        while (true){
+            try {
+                System.out.println("\uD83C\uDF2D \uD83C\uDF54 \uD83C\uDF5F \uD83C\uDF55 \uD83E\uDED3 \uD83E\uDD6A \uD83E\uDD59 \uD83E\uDDC6 \uD83C\uDF2E \uD83C\uDF2F");
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
