7cb9bab1c20869ed3bbfebf8d04bec994a64b59e
mamadou-wone <mamadu.wone@gmail.com>
2022-08-11 07:05:40 +0000

    ShellSort improve

--- amir/src/shellSort/ShellSort.java
+++ amir/src/shellSort/ShellSort.java
@@ -18,8 +18,25 @@ public class ShellSort {
         }
     }
 
+    public static void  shellSort(int[] intArray){
+        for(int gap = intArray.length / 2; gap > 0; gap /= 2){
+
+            for(int i = gap; i < intArray.length; i++){
+                int newElement = intArray[i];
+
+                int j = i;
+
+                while (j >= gap && intArray[j - gap] > newElement){
+                    intArray[j] = intArray[j - gap];
+                    j -= gap;
+                }
+                intArray[j] = newElement;
+            }
+        }
+    }
     public static void main(String[] args) {
-    //    insertionSort(myArray);
+//        insertionSort(myArray);
+        shellSort(myArray);
         for (int elem: myArray){
             System.out.println(elem);
         }
