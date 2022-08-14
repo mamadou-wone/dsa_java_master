dc1eba20e9e42c00ab0e56ae91dcdae3bd40698f
mamadou-wone <mamadu.wone@gmail.com>
2022-08-14 14:33:02 +0000

    String radix implementation

---
+++ amir/src/radixSort/StringRadixSort.java
@@ -0,0 +1,47 @@
+package radixSort;
+
+public class StringRadixSort {
+    /**
+     * initial Array that we need to sort
+     */
+    static String[] myArray = {"bcdef", "dbaqc", "abcde", "omadd", "bbbbb"};
+
+    public static void main(String[] args) {
+        radixSort(myArray, 26, 5);
+        for (String elem: myArray){
+            System.out.println(elem);
+        }
+    }
+
+    public static void radixSort(String[] input, int radix, int width){
+        for(int i = width - 1; i >= 0; i--){
+            radixSingleSort(input, i, radix);
+        }
+    }
+
+    public static void radixSingleSort(String[] input, int position, int radix){
+        int numItems = input.length;
+        int[] countArray = new int[radix];
+        for(String value: input){
+            countArray[getIndex(position, value)]++;
+        }
+        for(int j = 1; j < radix; j++){
+            countArray[j] += countArray[j - 1];
+        }
+
+        String[] temp = new String[numItems];
+        for(int tempIndex = numItems - 1; tempIndex >= 0; tempIndex--){
+            temp[--countArray[getIndex(position, input[tempIndex])]] = input[tempIndex];
+        }
+
+        //  System.arraycopy(temp, 0, input, 0, numItems);
+        for (int tempIndex = 0; tempIndex < numItems; tempIndex++){
+            input[tempIndex] = temp[tempIndex];
+        }
+    }
+
+    public static int getIndex(int position, String value){
+        return value.charAt(position) - 'a';
+    }
+
+}
