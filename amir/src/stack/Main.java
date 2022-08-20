11a4915068e4c0440adfe29aeafdbe2402c9054b
mamadou-wone <mamadu.wone@gmail.com>
2022-08-20 20:17:45 +0000

    Implement ArrayStack

--- amir/src/stack/Main.java
+++ amir/src/stack/Main.java
@@ -1,4 +1,21 @@
 package stack;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayStack stack = new ArrayStack(10);
+
+        stack.push(new Employee("Amir", "Boss", 1));
+        stack.push(new Employee("Gray", "Man", 2));
+        stack.push(new Employee("John", "Doe", 3));
+        stack.push(new Employee("Jeanne", "DOE", 4));
+
+    //    stack.printStack();
+
+        System.out.println(stack.peek());
+        System.out.println("Popped : " + stack.pop());
+        System.out.println(stack.peek());
+    }
+
 }
