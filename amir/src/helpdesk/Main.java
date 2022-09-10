76eee71ed465339e113e429463ca739feacbb685
mamadou-wone <mamadu.wone@gmail.com>
2022-09-10 18:19:17 +0000

    Add toLower and toUpper function

--- amir/src/helpdesk/Main.java
+++ amir/src/helpdesk/Main.java
@@ -3,7 +3,36 @@ package helpdesk;
 public class Main {
 
     public static void main(String[] args) {
-        getDigits(158946);
+    //    getDigits(158946);
+    //    System.out.println((int) ('A'));
+        System.out.println(toLower("Ws5NE"));
+        System.out.println(toUpper("wone472"));
+    }
+
+    public static String toUpper(String chain){
+        String chainToUpper = "";
+        char character;
+        for (int i = 0; i < chain.length(); i++) {
+            character = chain.charAt(i);
+            if (character >= 'a' && character <= 'z'){
+                character = (char)(character - 'a' + 'A');
+            }
+            chainToUpper += character;
+        }
+        return chainToUpper;
+    }
+
+    public static String toLower(String chain){
+        String chainToLower = "";
+        char character;
+        for (int i = 0; i < chain.length(); i++) {
+            character = chain.charAt(i);
+            if (character >= 'A' && character <= 'Z'){
+                character = (char)(character - 'A' + 'a');
+            }
+            chainToLower += character;
+        }
+        return chainToLower;
     }
 
     public static void getDigits(int number){
@@ -23,5 +52,7 @@ public class Main {
 //        }
     //    System.out.println("\nSum: " + sum);
     }
+
+
 
 }
